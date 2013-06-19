b4df8a8f576414a29587779990c0dc1e9ba6d5b5
Chad Whitacre <chad@zetaweb.com>
2013-06-19 08:19:41 -0400

    Turn off logging by default; #1
    This is really a debug log, causes probs when two users on one machine
    try to use assertEquals.

--- assertEquals/__init__.py
+++ assertEquals/__init__.py
@@ -4,7 +4,7 @@ import os
 __author__ = "Chad Whitacre <chad@zetaweb.com>"
 __version__ = "0.4"
 
-if 1:
+if 0:
     format = "%(name)-16s %(levelname)-8s %(message)s"
     format = "%(asctime)s %(module)s:%(lineno)d %(message)s"
     logging.basicConfig( filename='/tmp/assertEquals.log'
